5e4dd10ddbb2bd6708c9f8a5c86d321d2e6ce1be
Nicolò Pinciroli <nicolopinci.1997@gmail.com>
2020-02-08 01:04:41 +0100

    Windows to show different workspaces

--- addWorkspace.js
+++ addWorkspace.js
@@ -12,17 +12,36 @@ function addTab() {
 
 function createTabElement() {
   var workspaceTab = document.createElement("li");
-  workspaceTab.style.zIndex=document.getElementsByClassName("tabsGroup")[0].children[0].childElementCount*(-1)-100;
+  //workspaceTab.style.zIndex=document.getElementsByClassName("tabsGroup")[0].children[0].childElementCount*(-1)-100;
   workspaceTab.appendChild(document.createTextNode("Workspace "+document.getElementsByClassName("tabsGroup")[0].children[0].childElementCount));
   workspaceTab.id = "tab"+document.getElementsByClassName("tabsGroup")[0].children[0].childElementCount;
   workspaceTab.className ="ws";
+  workspaceTab.style.display = "none";
   document.getElementsByClassName("tabsGroup")[0].children[0].appendChild(workspaceTab);
 
   return workspaceTab;
 }
 
+
+
 function createContainerElement() {
 
+  body = document.getElementsByTagName("body")[0];
+
+  let dialogDiv = document.createElement("div");
+  dialogDiv.style = "min-width:400px; min-height:280px;";
+  dialogDiv.id = "dialog"+(document.getElementsByClassName("tabsGroup")[0].children[0].childElementCount-1);
+  dialogDiv.className = "dialog";
+  
+  let dialogTitlebar = document.createElement("div");
+  dialogTitlebar.className = "titlebar";
+  
+  let closeButton = document.createElement("button");
+  closeButton.name = "close";
+  
+  dialogDiv.appendChild(dialogTitlebar);
+  dialogDiv.appendChild(closeButton);
+  
   let container0 = document.getElementById("container");
 
   var newContainer = document.createElement("div");
@@ -30,11 +49,82 @@ function createContainerElement() {
   newContainer.className = "container";
   newContainer.style.display = "none";
   newContainer.id = "ctab"+(document.getElementsByClassName("tabsGroup")[0].children[0].childElementCount-1);
-  body = document.getElementsByTagName("body")[0];
-  body.appendChild(newContainer);
-
-  return newContainer;
+  
+  dialogDiv.appendChild(newContainer);
+  dialogDiv.style.display = "inherit";
+  body.appendChild(dialogDiv);
+  
+  dragElement(dialogDiv);
+  dialogDiv.addEventListener("click", activateFocus);
+  //dialogDiv.addEventListener("drag", activateFocus);
+    
+	return newContainer;
 }
+
+function activateFocus() {
+  let zIndexMax = 0;
+  for(var i=0; i<document.querySelectorAll(".dialog").length; ++i) {
+    if(parseInt(document.querySelectorAll(".dialog")[i].style.zIndex) > zIndexMax) {
+        zIndexMax = parseInt(document.querySelectorAll(".dialog")[i].style.zIndex);
+    }
+  }
+  this.style.zIndex = parseInt(zIndexMax+1);
+  
+  // Normalize indexes
+  let zIndexMin = Number.POSITIVE_INFINITY;
+  for(var i=0; i<document.querySelectorAll(".dialog").length; ++i) {
+    if(parseInt(document.querySelectorAll(".dialog")[i].style.zIndex) < zIndexMin) {
+        zIndexMin = parseInt(document.querySelectorAll(".dialog")[i].style.zIndex);
+    }
+  }
+  
+  for(var i=0; i<document.querySelectorAll(".dialog").length; ++i) {
+         document.querySelectorAll(".dialog")[i].style.zIndex = parseInt(document.querySelectorAll(".dialog")[i].style.zIndex - zIndexMin);
+  }
+}
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.querySelector("#"+elmnt.id + " .titlebar")) {
+    // if present, the header is where you move the DIV from:
+    document.querySelector("#"+elmnt.id + " .titlebar").onmousedown = dragMouseDown;
+    //document.getElementById(elmnt.id + "titlebar").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 
 function createPlusSign(containerID) {
   let plusLi = document.createElement("li"); // create a list element (as the other graphs)
@@ -219,7 +309,7 @@ function simulateClickOnTab(workspaceTab) {
 
 function openWorkspace(evt) {
   for(var i=0; i<document.querySelectorAll(".container").length; ++i) {
-    document.querySelectorAll(".container")[i].style.display = "none";
+    //document.querySelectorAll(".container")[i].style.zIndex--;
   }
   document.getElementById("c"+this.id).style.display = "inline-block";
 
@@ -417,7 +507,7 @@ function chooseDataset(evt) { // When a dataset is selected from the datasets li
   let tabID;
   let existingTabs = document.getElementsByClassName("container"); // all the tabs currently opened
   for(let t=0; t<existingTabs.length; ++t) { // for each tab
-    if(existingTabs[t].style.display == 'inline-block') { // the workspace is visible
+    if(this.closest("#"+existingTabs[t].id)!=null) { // the workspace is visible
       tabID = existingTabs[t].id; // remember the ID of that workspace
     }
   }
